8ae09648e9b4e958732d5d3c3b5d6730b04d1147
Bayu <bayu@rschooltoday.com>
2023-10-21 06:05:04 +0700

    Styling active link in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { SiDynatrace } from "react-icons/si" 
+import classNames from 'classnames'
 
 const NavBar = () => {
     const links = [
@@ -8,15 +12,24 @@ const NavBar = () => {
         {href: "/issues", label: "Issues"}
     ];
 
-  return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/" className="flex space-x-6"><SiDynatrace /></Link>
-        <ul className='flex space-x-6'>
-            {links.map(link => <Link key={link.key} href={link.href} className="flex space-x-6 text-zinc-500 hover:text-zinc-800 transition-color">{link.label}</Link>)}
-        </ul>
+    const activePath = usePathname();
+    console.log(activePath);
 
-    </nav>
-  )
+    return (
+        <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+            <Link href="/" className="flex space-x-6"><SiDynatrace /></Link>
+            <ul className='flex space-x-6'>
+                {links.map(link => 
+                    <Link key={link.href} href={link.href} className={classNames({
+                        'text-zinc-900': activePath === link.href,
+                        'text-zinc-500': activePath !== link.href,
+                        'flex space-x-6 hover:text-zinc-800 transition-color':true
+                    })}>{link.label}</Link>
+                )}
+            </ul>
+
+        </nav>
+    )
 }
 
 export default NavBar
