dcbd03856aca8bcda29844477ff12e2ad216dbfd
Bayu <bayu@rschooltoday.com>
2023-11-01 15:08:52 +0700

    Add metadata to the page

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -4,6 +4,7 @@ import { Status } from '@prisma/client'
 import IssueTable, { IssueQuery, columnNames } from './IssueTable'
 import IssueToolbar from './IssueToolbar'
 import { Flex } from '@radix-ui/themes'
+import { Metadata } from 'next'
 
 interface Props {
   searchParams: IssueQuery
@@ -44,3 +45,8 @@ const IssuePage = async ({ searchParams }: Props) => {
 }
 
 export default IssuePage
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - Issue List",
+  description: "View all project issues"
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import IssueSummary from "./IssueSummary"
 import LatestIssues from "./LatestIssues"
 import IssueChart from "./IssueChart"
 import { Flex, Grid } from "@radix-ui/themes"
+import { Metadata } from "next"
 
 
 export default async function Home({ searchParams }: {
@@ -24,4 +25,9 @@ export default async function Home({ searchParams }: {
       <LatestIssues />
     </Grid>
   )
+}
+
+export const metadata: Metadata = {
+    title: "Issue Tracker - Dashboard",
+    description: 'View a summary of issues'
 }
